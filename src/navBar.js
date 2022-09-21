4048d9bb8a6e1a2243b9c7ac628a4ec91deeb1f3
Fdamirez <fabianramirez0072@gmail.com>
2022-09-20 21:16:52 -0500

    Firs

---
+++ src/navBar.js
@@ -0,0 +1,11 @@
+export default function NavBar() {
+  return (
+    <>
+      <h1>opciones del navegador</h1>
+      <h5>Opc 1</h5>
+      <h5>Opc 2</h5>
+      <h5>Opc 3</h5>
+      <h5>Opc 4</h5>
+    </>
+  );
+}
